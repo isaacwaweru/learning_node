56c861ba2b880edb36ecb858c34e901c53014efc
isaacwaweru <waweruisaac25@gmail.com>
2023-03-15 23:12:19 +0300

    initial commit

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -8,3 +8,21 @@ router
   .post(todoController.createTodo);
 
 module.exports = router;
+
+// const express = require('express');
+// const todoController = require('../controllers/todoController');
+
+// const router = express.Router();
+
+// router
+//   .route('/')
+//   .get(todoController.getAllTodos)
+//   .post(todoController.createTodo);
+
+// router
+//   .route('/:id')
+//   .get(todoController.getTodo)
+//   .patch(todoController.updateTodo)
+//   .delete(todoController.deleteTodo);
+
+// module.exports = router;
