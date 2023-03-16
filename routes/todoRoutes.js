c87642eba9caf7ba987639c4d1606c609c9197ce
Sarah Mukuti <cesarwrites24@gmail.com>
2023-03-16 18:33:20 +0300

    add update and delete routes

--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -16,6 +16,16 @@ router
    .route('/:id')
    .get(todoController.getTodo)
 
+  
+router
+  .route('/:id')
+  .patch(todoController.updateTodo);
+
+router
+  .route('/:id')
+  .delete(todoController.deleteTodo);
+
+  
 module.exports = router;
 
 // const express = require('express');
