0689c1216c8b09ee052e08b6e28972425ed90d2e
isaacwaweru <waweruisaac25@gmail.com>
2023-03-16 22:44:10 +0300

    todo crud

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,19 +1,59 @@
-const Todo = require('../models/todoModel');
+const Todo = require("../models/todoModel");
 
+// Create a todo
 exports.createTodo = async (req, res, next) => {
   try {
-    const newTodo = await Todo.create({
-    name: req.body.name,
-    duration: req.body.duration,
-  });
-    res.status(200).json(newTodo);
+    const data = await Todo.create({
+      name: req.body.name,
+      duration: req.body.duration,
+    });
+
+    res.status(200).json(data);
   } catch (error) {
     res.status(400).json(error);
   }
 };
 
-// exports.createTodo = factory.createOne(Todo);
-// exports.getTodo = factory.getOne(Todo);
-// exports.getAllTodos = factory.getAll(Todo);
-// exports.updateTodo = factory.updateOne(Todo);
-// exports.deleteTodo = factory.deleteOne(Todo);
+// Get a todo
+exports.getTodo = async (req, res) => {
+  try {
+    const todo = await Todo.findById(req.params.id);
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+// Get all todos
+exports.getAllTodos = async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+// Update a todo
+exports.updateTodo = async (req, res) => {
+  try {
+    const todos = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+      useFindAndModify: false,
+    });
+    res.status(200).json(todos);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+// Delete a todo
+exports.deleteTodo = async (req, res) => {
+  try {
+    const todos = await Todo.findByIdAndRemove(req.params.id);
+    res.status(200).json({
+      status: "success"
+    });
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,28 +1,18 @@
-const express = require('express');
-const todoController = require('../controllers/todoController');
+const express = require("express");
+const todoController = require("../controllers/todoController");
 
 const router = express.Router();
 
 router
-  .route('/')
-  .post(todoController.createTodo);
+  .route("/")
+  .post(todoController.createTodo)
+  .get(todoController.getAllTodos);
+
+router
+  .route("/:id")
+  .get(todoController.getTodo)
+  .patch(todoController.updateTodo)
+  .delete(todoController.deleteTodo);
 
 module.exports = router;
 
-// const express = require('express');
-// const todoController = require('../controllers/todoController');
-
-// const router = express.Router();
-
-// router
-//   .route('/')
-//   .get(todoController.getAllTodos)
-//   .post(todoController.createTodo);
-
-// router
-//   .route('/:id')
-//   .get(todoController.getTodo)
-//   .patch(todoController.updateTodo)
-//   .delete(todoController.deleteTodo);
-
-// module.exports = router;
