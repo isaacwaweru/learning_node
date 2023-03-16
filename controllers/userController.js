4038b7bfbc13d399241d4af611759874a12ae3b8
Sarah Mukuti <cesarwrites24@gmail.com>
2023-03-16 21:51:46 +0300

    modify update controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -13,7 +13,7 @@ exports.createUser = async (req, res, next) => {
     }
   };
 
-  //Get a single user
+  // Get a single user
   exports.getUser = async ( req, res ) => {
     const id = req.params.id;
 
@@ -47,3 +47,27 @@ exports.getAllUsers = async (req, res) => {
       });
     });
 };
+
+// Update user details
+exports.updateUser = async (req, res) => {
+    if (!req.body) {
+        return res.status(400).send({
+            message: "Data to update can not be empty!"
+        });
+    }
+    const id = req.params.id;
+  
+    await User.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+        .then(data => {
+            if (!data) {
+                res.status(404).send({
+                    message: `Cannot update user with id=${id} Maybe Tutorial was not found!`
+                });
+            } else res.send({ message: "User was updated successfully" });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating User with id=" +id
+            });
+        });
+  };
---
+++ routes/userRoutes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const userController = require('../controllers/userController');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(userController.createUser);
+
+router
+  .route('/')
+   .get(userController.getAllUsers)
+   .post(userController.createUser);
+
+ router
+   .route('/:id')
+   .get(userController.getUser)
+
+module.exports = router;
