d7dfba5cb8421272d620b808a43e52c8ef55a04a
Sarah Mukuti <cesarwrites24@gmail.com>
2023-03-16 13:32:14 +0300

    add get and post controllers

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -14,6 +14,40 @@ exports.createTodo = async (req, res, next) => {
 
 // exports.createTodo = factory.createOne(Todo);
 // exports.getTodo = factory.getOne(Todo);
+
+exports.getTodo = async ( req, res ) => {
+    const id = req.params.id;
+
+    await Todo.findById(id)
+    .then(data => {
+      if ( !data )
+        res.status(404).send({ message: "Not found tutorial with id" + id });
+        else res.send(data);
+    } )
+    .catch(err => {
+      res
+       .status(500)
+       .send({ message: "Error retrieving Tutorial with id=" +id });
+    });
+  }
 // exports.getAllTodos = factory.getAll(Todo);
+
+exports.getAllTodos = (req, res) => {
+  const  name = req.query.name;
+  var condition = name ? { title: { $regex: new RegExp(name), $options: "i" } } : {};
+
+  Todo.find(condition)
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving todos."
+      });
+    });
+};
+
 // exports.updateTodo = factory.updateOne(Todo);
 // exports.deleteTodo = factory.deleteOne(Todo);
+
