d064a90ffe471b7c1124020bfcd3b06d67f3eb4d
Sarah Mukuti <cesarwrites24@gmail.com>
2023-03-17 00:44:24 +0300

    modify user controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -5,7 +5,7 @@ exports.createUser = async (req, res, next) => {
       const newUser = await User.create({
       name: req.body.name,
       email: req.body.email,
-      password: req.body.password,
+      phone: req.body.phone,
     });
       res.status(200).json(newUser);
     } catch (error) {
@@ -13,6 +13,50 @@ exports.createUser = async (req, res, next) => {
     }
   };
 
+// get a single user
+  exports.getUser = async ( req, res ) => {
+    try {
+      const user = await User.findById(req.params.id);
+      res.status(200).json(todo);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+
+// get all users
+  exports.getAllUsers = async (req, res) => {
+    try {
+      const users = await User.find();
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  }
+// Update users
+exports.updateUser = async (req, res) => {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        useFindAndModify: false,
+      });
+      res.status(200).json(users);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  };
+
+// Delete user
+exports.deleteUser = async (req, res) => {
+    try {
+      const user = await User.findByIdAndRemove(req.params.id);
+      res.status(200).json({
+        status: "successfully deleted"
+      });
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  };
+ 
+  /*
   // Get a single user
   exports.getUser = async ( req, res ) => {
     const id = req.params.id;
@@ -94,3 +138,4 @@ exports.updateUser = async (req, res) => {
       });
     });
   };
+  */
