fb13f7742d99dacbdaf2be364f843d62687641d9
monginadiana <dianahmongina2@gmail.com>
2023-03-20 21:47:55 +0300

    use of catchasync

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,62 +1,96 @@
-const Todo = require("../models/todoModel");
+const Todo = require('../models/todoModel');
+const catchAsync = require('../utils/catchAsync');
 
-exports.createTodo = async (req, res, next) => {
-  try {
-    const newTodo = await Todo.create({
-      name: req.body.name,
-      duration: req.body.duration,
-    });
-    res.status(200).json(newTodo);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
+// exports.createTodo = async (req, res, next) => {
+//   try {
+//     const newTodo = await Todo.create({
+//       name: req.body.name,
+//       duration: req.body.duration,
+//     });
+//     res.status(200).json(newTodo);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
 
-// Get a todo
+// // Get a todo
 
-exports.getTodo = async (req, res) => {
-  try {
-    const todo = await Todo.findById(req.params.id);
-    res.status(200).json(todo);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
+// exports.getTodo = async (req, res) => {
+//   try {
+//     const todo = await Todo.findById(req.params.id);
+//     res.status(200).json(todo);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
 
-exports.getAllTodos = async (req, res) => {
-  try {
-    const todos = await Todo.find();
-    res.status(200).json(todos);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
+// exports.getAllTodos = async (req, res) => {
+//   try {
+//     const todos = await Todo.find();
+//     res.status(200).json(todos);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
 
-exports.updateTodo = async (req, res) => {
-  try {
-    const todos = await Todo.findById(req.params.id, req.body, {
-      useFindAndModify: false,
-    });
-    res.status(200).json(todos);
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
+// exports.updateTodo = async (req, res) => {
+//   try {
+//     const todos = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+//       useFindAndModify: false,
+//     });
+//     res.status(200).json(todos);
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
 
-exports.deleteTodo = async (req, res) => {
-  try {
-    const todos = await Todo.findById(req.params.id);
+// exports.deleteTodo = async (req, res) => {
+//   try {
+//     await Todo.findById(req.params.id);
 
-    res.status(200).json({
-      status: "success",
-    });
-  } catch (error) {
-    res.status(400).json(error);
-  }
-};
+//     res.status(200).json({
+//       status: 'success',
+//     });
+//   } catch (error) {
+//     res.status(400).json(error);
+//   }
+// };
 
 // exports.createTodo = factory.createOne(Todo);
 // exports.getTodo = factory.getOne(Todo);
 // exports.getAllTodos = factory.getAll(Todo);
 // exports.updateTodo = factory.updateOne(Todo);
 // exports.deleteTodo = factory.deleteOne(Todo);
+
+//create to do
+exports.createTodo = catchAsync(async (req, res, next) => {
+  const newTodo = await Todo.create({
+    name: req.body.name,
+    duration: req.body.duration,
+  });
+  res.status(200).json(newTodo);
+});
+
+exports.getTodo = catchAsync(async (req, res) => {
+  const todo = await Todo.findById(req.params.id);
+  res.status(200).json(todo);
+});
+
+exports.getAllTodos = catchAsync(async (req, res) => {
+  const todos = await Todo.find();
+  res.status(200).json(todos);
+});
+
+exports.updateTodo = catchAsync(async (req, res) => {
+  const todos = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+    useFindAndModify: false,
+  });
+  res.status(200).json(todos);
+});
+
+exports.deleteTodo = catchAsync(async (req, res) => {
+  await Todo.findById(req.params.id);
+  res.status(200).json({
+    status: 'success',
+  });
+});
--- routes/todoRoutes.js
+++ routes/todoRoutes.js
@@ -1,45 +1,18 @@
 const express = require('express');
 const todoController = require('../controllers/todoController');
-const Todo = require('../models/todoModel');
+// const Todo = require('../models/todoModel');
 
 const router = express.Router();
 
 router
   .route('/')
-  .post(todoController.createTodo);
+  .post(todoController.createTodo)
+  .get(todoController.getAllTodos);
 
 router
-    .route('/:id')
-    .get(todoController.getTodo)
-
-router 
-    .route('/')
-    .get(todoController.getAllTodos)
-
-router 
-    .route('/:id')
-    .put(todoController.updateTodo)
-
-router 
-    .route('/:id')
-    .delete(todoController.deleteTodo)
+  .route('/:id')
+  .get(todoController.getTodo)
+  .patch(todoController.updateTodo)
+  .delete(todoController.deleteTodo);
 
 module.exports = router;
-
-// const express = require('express');
-// const todoController = require('../controllers/todoController');
-
-// const router = express.Router();
-
-// router
-//   .route('/')
-//   .get(todoController.getAllTodos)
-//   .post(todoController.createTodo);
-
-// router
-//   .route('/:id')
-//   .get(todoController.getTodo)
-//   .patch(todoController.updateTodo)
-//   .delete(todoController.deleteTodo);
-
-// module.exports = router;
