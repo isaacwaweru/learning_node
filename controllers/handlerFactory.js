12c49f09ab8ba369bcc0f4421ae9d694eac8572a
monginadiana <dianahmongina2@gmail.com>
2023-03-21 06:56:45 +0300

    added handle factory for my todo app

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,81 @@
+const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+exports.deleteOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const todo = await Model.findByIdAndDelete(req.params.id);
+
+    if (!todo) {
+      return next(new AppError('No todo found with that ID', 404));
+    }
+
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
+
+exports.updateOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const todo = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (todo) {
+      return next(new AppError('No todo found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: todo,
+      },
+    });
+  });
+
+exports.createOne = Model =>
+  catchAsync(async (req, res, next) => {
+    const todo = await Model.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        data: todo,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+    const todo = await query;
+
+    if (!todo) {
+      return next(new AppError('No todo found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        data: todo,
+      },
+    });
+  });
+
+exports.getAll = Model =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested GET reviews on tour (hack)
+
+    const getAllList = await Model.find();
+
+    // SEND RESPONSE
+    res.status(200).json({
+      status: 'success',
+      getRecords: getAllList.length,
+      data: {
+        data: getAllList,
+      },
+    });
+  });
--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,5 +1,12 @@
 const Todo = require('../models/todoModel');
-const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+
+// Create a todo
+exports.createTodo = factory.createOne(Todo);
+exports.getTodo = factory.getOne(Todo);
+exports.getAllTodos = factory.getAll(Todo);
+exports.updateTodo = factory.updateOne(Todo);
+exports.deleteTodo = factory.deleteOne(Todo);
 
 // exports.createTodo = async (req, res, next) => {
 //   try {
@@ -63,34 +70,34 @@ const catchAsync = require('../utils/catchAsync');
 // exports.deleteTodo = factory.deleteOne(Todo);
 
 //create to do
-exports.createTodo = catchAsync(async (req, res, next) => {
-  const newTodo = await Todo.create({
-    name: req.body.name,
-    duration: req.body.duration,
-  });
-  res.status(200).json(newTodo);
-});
+// exports.createTodo = catchAsync(async (req, res, next) => {
+//   const newTodo = await Todo.create({
+//     name: req.body.name,
+//     duration: req.body.duration,
+//   });
+//   res.status(200).json(newTodo);
+// });
 
-exports.getTodo = catchAsync(async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  res.status(200).json(todo);
-});
+// exports.getTodo = catchAsync(async (req, res) => {
+//   const todo = await Todo.findById(req.params.id);
+//   res.status(200).json(todo);
+// });
 
-exports.getAllTodos = catchAsync(async (req, res) => {
-  const todos = await Todo.find();
-  res.status(200).json(todos);
-});
+// exports.getAllTodos = catchAsync(async (req, res) => {
+//   const todos = await Todo.find();
+//   res.status(200).json(todos);
+// });
 
-exports.updateTodo = catchAsync(async (req, res) => {
-  const todos = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-    useFindAndModify: false,
-  });
-  res.status(200).json(todos);
-});
+// exports.updateTodo = catchAsync(async (req, res) => {
+//   const todos = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+//     useFindAndModify: false,
+//   });
+//   res.status(200).json(todos);
+// });
 
-exports.deleteTodo = catchAsync(async (req, res) => {
-  await Todo.findById(req.params.id);
-  res.status(200).json({
-    status: 'success',
-  });
-});
+// exports.deleteTodo = catchAsync(async (req, res) => {
+//   await Todo.findById(req.params.id);
+//   res.status(200).json({
+//     status: 'success',
+//   });
+// });
