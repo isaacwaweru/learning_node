daa8de13c2e2414fbc737d6f35712b4d74fab726
Mainamathenge <mainamathengej@gmail.com>
2023-03-21 19:50:40 +0300

    added_handler_to_todo

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,40 +1,48 @@
 const Todo = require('../models/todoModel');
-const catchAsync = require('../utils/catchAsync');
+const factory = require('./handlerFactory');
+//const catchAsync = require('../utils/catchAsync');
 //create to do
-exports.createTodo = catchAsync(async (req, res, next) => {
-  const data = await Todo.create({
-    name: req.body.name,
-    duration: req.body.duration,
-  });
-  res.status(200).json(data);
-});
 
-exports.getTodo = catchAsync(async (req, res) => {
-  const todo = await Todo.findById(req.params.id);
-  res.status(200).json(todo);
-});
+exports.createJoke = factory.createOne(Todo);
+exports.getJoke = factory.getOne(Todo);
+exports.getAllJokes = factory.getAll(Todo);
+exports.updateJoke = factory.updateOne(Todo);
+exports.deleteJoke = factory.deleteOne(Todo);
 
-exports.getAllTodos = catchAsync(async (req, res) => {
-  const { name } = req.query;
-  const condition = name
-    ? { title: { $regex: new RegExp(name), $options: 'i' } }
-    : {};
-  await Todo.find(condition).then(data => {
-    res.send(data);
-  });
-});
-// exports.updateTodo = factory.updateOne(Todo);
-exports.updateTodo = catchAsync(async (req, res) => {
-  const todos = await Todo.findByIdAndUpdate(req.params.id, req.body, {
-    useFindAndModify: false,
-  });
-  res.status(200).json(todos);
-});
-// exports.deleteTodo
+// exports.createTodo = catchAsync(async (req, res, next) => {
+//   const data = await Todo.create({
+//     name: req.body.name,
+//     duration: req.body.duration,
+//   });
+//   res.status(200).json(data);
+// });
 
-exports.deleteTodo = catchAsync(async (req, res) => {
-  await Todo.findByIdAndRemove(req.params.id);
-  res.status(200).json({
-    status: 'success',
-  });
-});
+// exports.getTodo = catchAsync(async (req, res) => {
+//   const todo = await Todo.findById(req.params.id);
+//   res.status(200).json(todo);
+// });
+
+// exports.getAllTodos = catchAsync(async (req, res) => {
+//   const { name } = req.query; 
+//   const condition = name
+//     ? { title: { $regex: new RegExp(name), $options: 'i' } }
+//     : {};
+//   await Todo.find(condition).then(data => {
+//     res.send(data);
+//   });
+// });
+// // exports.updateTodo = factory.updateOne(Todo);
+// exports.updateTodo = catchAsync(async (req, res) => {
+//   const todos = await Todo.findByIdAndUpdate(req.params.id, req.body, {
+//     useFindAndModify: false,
+//   });
+//   res.status(200).json(todos);
+// });
+// // exports.deleteTodo
+
+// exports.deleteTodo = catchAsync(async (req, res) => {
+//   await Todo.findByIdAndRemove(req.params.id);
+//   res.status(200).json({
+//     status: 'success',
+//   });
+// });
