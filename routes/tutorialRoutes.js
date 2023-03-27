2f07193ab2d0fc084b0015ab3c31d32f72e30e7e
monginadiana <dianahmongina2@gmail.com>
2023-03-27 16:57:25 +0300

    protect

--- routes/tutorialRoutes.js
+++ routes/tutorialRoutes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const tutorialController = require('../controllers/tutorialController');
 // const Todo = require('../models/todoModel');
 
+const authController = require('../controllers/authController');
+
 const router = express.Router();
+
+// Protect all routes after this middleware
+router.use(authController.protect);
 
 router
   .route('/')
