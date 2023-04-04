bf48643429ac7da0b87ebc5242d6de9f126922af
isaacwaweru <waweruisaac25@gmail.com>
2023-04-04 16:11:55 +0300

    added global error handling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const viewController = require('./routes/viewRoutes');
 const userRouter = require('./routes/userRoutes');
 const todoRouter = require('./routes/todoRoutes');
@@ -31,5 +32,7 @@ app.use('/api/v1/coaches', coachRouter);
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
+
+app.use(globalErrorHandler);
 
 module.exports = app;
